eb9d8d870a7fb0e68cf031032a1216543b7d4758
Farnaz <farnaz@sphereishere.com>
2021-07-28 13:20:05 -0700

    Add PORT as env var

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
 
 //Here we store env variables and config variables.
 module.exports = {
-  port: 8080,
+  port: process.env.PORT || 8080,
   mongoDbName: process.env.MONGODB_NAME,
   mongodbConnections: process.env.MONGODB_CONNECTIONS || 'mongodb://localhost:27017',
 }
