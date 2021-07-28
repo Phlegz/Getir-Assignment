169995d4dcb28f1fe4f1cfaf8bb132fdd99eef1d
Farnaz <farnaz@sphereishere.com>
2021-07-28 13:00:31 -0700

    Add mongoDb connector

---
+++ data/mongoConnector.js
@@ -0,0 +1,21 @@
+const { MongoClient } = require('mongodb');
+const config = require('../config');
+
+let connection = null;
+
+//Initialize connection once and reuse the connection if it already exists.
+async function getMongoConnection () {
+  if (connection && connection?.constructor.name === 'MongoClient') {
+    return connection;
+  }
+  try {
+    connection = await MongoClient.connect(
+      config.mongodbConnections, 
+    )
+    return connection;
+  } catch(error) {
+      console.log('Error in establishing a connection to Mongo server');
+  }
+}
+
+module.exports.getMongoConnection = getMongoConnection;
