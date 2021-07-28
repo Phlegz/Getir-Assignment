cd89e295011168936e38509f9ce78396425a4a8b
Farnaz <farnaz@sphereishere.com>
2021-07-28 13:04:06 -0700

    Add helper function to get mongoDb connection

---
+++ controllers/helpers.js
@@ -0,0 +1,20 @@
+const { getMongoConnection } = require('../data/mongoConnector');
+const config = require('../config');
+
+//***************************************** Helper functions ************************************************//
+let client;
+
+async function getConnection() {
+  try {
+    if (!client) {
+      client = await getMongoConnection();
+    }
+    return { db: client.db(config.mongoDbName).collection('records') };
+  } catch (err) {
+    console.log('error', err)
+    return false;
+  }
+}
+
+//********************************************* Exports ****************************************************//
+module.exports.getConnection = getConnection;
